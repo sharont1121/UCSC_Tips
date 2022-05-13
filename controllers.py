22f259a00896804c7a3256731342891e8468c9e5
1832051637 <z1832051637@gmail.com>
2022-05-13 13:10:57 -0700

    Testing Random coordinate generator

--- controllers.py
+++ controllers.py
@@ -42,21 +42,22 @@ from .common import (
 from py4web.utils.url_signer import URLSigner
 from .models import get_user_email, get_user_id, get_terms_from_str
 from .param_parser import ParamParser, BoolParam
-from .random_data import add_fake_data, int_to_color
+from .random_data import add_fake_data, int_to_color, generate_random_coord
 
 import random
 
 url_signer = URLSigner(session)
 
-def user_profile_url():
-    return URL('profile', get_user_id())
 
-@action('index')
-@action.uses('index.html', db, auth)
+def user_profile_url():
+    return URL("profile", get_user_id())
+
+
+@action("index")
+@action.uses("index.html", db, auth)
 def index():
     print("User:", get_user_email())
-    return dict(
-    )
+    return dict()
 
 
 @action("feed")
@@ -64,8 +65,9 @@ def index():
 def feed():
     expected_param_types = {"missing": BoolParam}  # exludes string types
     params = ParamParser(request.params, expected_param_types)
+    print(generate_random_coord())
     return dict(
-        base_load_posts_url=URL('feed', 'load'),
+        base_load_posts_url=URL("feed", "load"),
         load_posts_url=URL(
             "feed", "load", vars=params.dict_of(["selectedid", "search", "tags"])
         ),
@@ -92,16 +94,16 @@ def add_data():
     redirect(URL("feed"))
 
 
-def get_posts(db, query, tags = None, **kwargs):
+def get_posts(db, query, tags=None, **kwargs):
     tag1 = db.tags.with_alias("tag1")
     tag2 = db.tags.with_alias("tag2")
     tag3 = db.tags.with_alias("tag3")
     user = db.auth_user
     if tags:
         query = query & (
-            tag1.tag_name.belongs(tags) |
-            tag2.tag_name.belongs(tags) |
-            tag3.tag_name.belongs(tags)
+            tag1.tag_name.belongs(tags)
+            | tag2.tag_name.belongs(tags)
+            | tag3.tag_name.belongs(tags)
         )
     res = db(query).select(
         db.posts.ALL,
@@ -131,37 +133,39 @@ def search_posts(db, search_terms, tags=None, limitby=None, exclude=[]):
 
     num_posts = db(db.posts).count()
 
-    div = ((db.term_freq.post_freq * db.posts.inverse_max_freq * num_posts) / db.terms.doc_freq)
+    div = (
+        db.term_freq.post_freq * db.posts.inverse_max_freq * num_posts
+    ) / db.terms.doc_freq
 
-    query = (db.terms.term.belongs(search_terms) & ~db.posts.id.belongs(exclude) )
-    
+    query = db.terms.term.belongs(search_terms) & ~db.posts.id.belongs(exclude)
+
     if tags:
         query = query & (
-            tag1.tag_name.belongs(tags) |
-            tag2.tag_name.belongs(tags) |
-            tag3.tag_name.belongs(tags)
+            tag1.tag_name.belongs(tags)
+            | tag2.tag_name.belongs(tags)
+            | tag3.tag_name.belongs(tags)
         )
     relevent_terms = db(query).select(
-            div,
-            db.posts.ALL,
-            tag1.ALL,
-            tag2.ALL,
-            tag3.ALL,
-            user.id,
-            user.first_name,
-            orderby=~div,
-            groupby=db.posts.id,
-            having= (div > 0.0),
-            limitby=limitby,
-            left=[
-                db.terms.on(db.term_freq.term == db.terms.id),
-                db.posts.on(db.posts.id == db.term_freq.post),
-                tag1.on(db.posts.tag1 == tag1.id),
-                tag2.on(db.posts.tag2 == tag2.id),
-                tag3.on(db.posts.tag3 == tag3.id),
-                user.on(db.posts.created_by == user.id),
-            ],
-        )
+        div,
+        db.posts.ALL,
+        tag1.ALL,
+        tag2.ALL,
+        tag3.ALL,
+        user.id,
+        user.first_name,
+        orderby=~div,
+        groupby=db.posts.id,
+        having=(div > 0.0),
+        limitby=limitby,
+        left=[
+            db.terms.on(db.term_freq.term == db.terms.id),
+            db.posts.on(db.posts.id == db.term_freq.post),
+            tag1.on(db.posts.tag1 == tag1.id),
+            tag2.on(db.posts.tag2 == tag2.id),
+            tag3.on(db.posts.tag3 == tag3.id),
+            user.on(db.posts.created_by == user.id),
+        ],
+    )
     return relevent_terms
 
 
@@ -172,10 +176,10 @@ DEFAULT_POST_COUNT = 10
 @action.uses(db, auth)
 def feed_load():
     expected_param_types = {
-        "min": int, 
-        "max": int, 
-        "selectedid": int, 
-        "search": str, 
+        "min": int,
+        "max": int,
+        "selectedid": int,
+        "search": str,
         "tags": JSON,
         "userid": int,
     }
@@ -184,7 +188,7 @@ def feed_load():
     min_post = params.min or 0
     max_post = params.max or (min_post + DEFAULT_POST_COUNT)
 
-    query = (db.posts.id != params.selectedid)
+    query = db.posts.id != params.selectedid
     data = []
 
     post = get_posts(db, query=db.posts.id == params.selectedid, limitby=(0, 1))
@@ -198,16 +202,19 @@ def feed_load():
         query = query & db.posts.title.ilike(f"%{params.search}%")
 
     posts = get_posts(
-            db, query=query, tags=params.tags, orderby=~db.posts.rating, limitby=(min_post, max_post)
-        )
-
-    data.extend(
-        posts
+        db,
+        query=query,
+        tags=params.tags,
+        orderby=~db.posts.rating,
+        limitby=(min_post, max_post),
     )
+
+    data.extend(posts)
 
     if params.search and (len(data) + min_post) < max_post:
         ids = [
-            r.id for r in db(query).select(
+            r.id
+            for r in db(query).select(
                 db.posts.id, orderby=~db.posts.rating, limitby=(0, max_post)
             )
         ]
@@ -216,7 +223,13 @@ def feed_load():
         terms = [k for k in get_terms_from_str(params.search)]
 
         data.extend(
-            search_posts(db, terms, tags=params.tags, limitby=(len(data) + min_post, max_post), exclude=ids)
+            search_posts(
+                db,
+                terms,
+                tags=params.tags,
+                limitby=(len(data) + min_post, max_post),
+                exclude=ids,
+            )
         )
 
     return dict(data=data, selectedid=params.selectedid, missing=missing)
@@ -238,9 +251,8 @@ def profile(uid):
 @action("create_post")
 @action.uses("create_post.html", url_signer, auth.user)
 def create_post():
-    return dict(
-        add_tip_url=URL('add_tip', signer=url_signer),
-    )
+    return dict(add_tip_url=URL("add_tip", signer=url_signer),)
+
 
 @action("add_tip", method="POST")
 @action.uses(url_signer.verify(), db)
@@ -251,60 +263,45 @@ def add_tip():
     if request.json.get("tag1_name") == "":
         tag1_id = None
     elif len(t1) == 0:
-        tag1_id = db.tags.insert(
-            tag_name=request.json.get("tag1_name"),
-            uses=1
-        )
+        tag1_id = db.tags.insert(tag_name=request.json.get("tag1_name"), uses=1)
     # Otherwise increment its 'uses' field
     else:
         tag1_id = t1[0]["id"]
         tag1 = db.tags[tag1_id]
-        db(
-            db.tags.id == tag1_id
-        ).update(uses=tag1.uses + 1)
+        db(db.tags.id == tag1_id).update(uses=tag1.uses + 1)
 
     # If tag2 doesn't exist in the db, insert it and set uses to 1
     t2 = db(db.tags.tag_name == request.json.get("tag2_name")).select().as_list()
     if request.json.get("tag2_name") == "":
         tag2_id = None
     elif len(t2) == 0:
-        tag2_id = db.tags.insert(
-            tag_name=request.json.get("tag2_name"),
-            uses=1
-        )
+        tag2_id = db.tags.insert(tag_name=request.json.get("tag2_name"), uses=1)
     # Otherwise increment its 'uses' field
     else:
         tag2_id = t2[0]["id"]
         tag2 = db.tags[tag2_id]
-        db(
-            db.tags.id == tag2_id
-        ).update(uses=tag2.uses + 1)
+        db(db.tags.id == tag2_id).update(uses=tag2.uses + 1)
 
     # If tag3 doesn't exist in the db, insert it and set uses to 1
     t3 = db(db.tags.tag_name == request.json.get("tag3_name")).select().as_list()
     if request.json.get("tag3_name") == "":
         tag3_id = None
     elif len(t3) == 0:
-        tag3_id = db.tags.insert(
-            tag_name=request.json.get("tag3_name"),
-            uses=1
-        )
+        tag3_id = db.tags.insert(tag_name=request.json.get("tag3_name"), uses=1)
     # Otherwise increment its 'uses' field
     else:
         tag3_id = t3[0]["id"]
         tag3 = db.tags[tag3_id]
-        db(
-            db.tags.id == tag3_id
-        ).update(uses=tag3.uses + 1)
+        db(db.tags.id == tag3_id).update(uses=tag3.uses + 1)
 
     # Using the ids of the tags, now insert the post into the db
     id = db.posts.insert(
-       title=request.json.get("title"),
-       body=request.json.get("body"),
-       created_by=get_user_id(),
-       tag1=tag1_id,
-       tag2=tag2_id,
-       tag3=tag3_id,
+        title=request.json.get("title"),
+        body=request.json.get("body"),
+        created_by=get_user_id(),
+        tag1=tag1_id,
+        tag2=tag2_id,
+        tag3=tag3_id,
     )
 
     print("ID of the post created: ", id)
--- random_data.py
+++ random_data.py
@@ -1,36 +1,54 @@
 import pathlib
-from random import choice, choices, randint, random
+from random import choice, choices, randint, random, uniform
 
 from .settings import APP_FOLDER
+
 BOOK = []
+
+santa_cruz_lat = [36.947100, 37.014780]
+santa_cruz_lon = [-122.076839, -121.984772]
+
+
+def generate_random_coord():
+    lat = uniform(santa_cruz_lat[0], santa_cruz_lat[1])
+    lon = uniform(santa_cruz_lon[0], santa_cruz_lon[1])
+    print(lat, lon)
+    return lat, lon
+    # pass
+
 
 def generate_shelly_text(n):
     if not BOOK:
         print()
-        with open(pathlib.Path(APP_FOLDER).joinpath('frankenstein.txt'), 'r', encoding="utf-8") as book:
+        with open(
+            pathlib.Path(APP_FOLDER).joinpath("frankenstein.txt"), "r", encoding="utf-8"
+        ) as book:
             full_text = book.read()
             for word in full_text.split():
                 BOOK.append(word)
-    return " ".join(choices(BOOK,k=n))
+    return " ".join(choices(BOOK, k=n))
 
 
 def generate_garbage_word(num_chars: int):
-    LETTERS = [ a for a in "abcdefghijklmnopqrstuvwxyz"]
-    res= ""
+    LETTERS = [a for a in "abcdefghijklmnopqrstuvwxyz"]
+    res = ""
     for _ in range(num_chars):
         res += choice(LETTERS)
     return res
-def generate_garbage_text(num_words: int, max_chars = None):
-    WORD_LEN_FREQ = [1,2,3,3,4,4,4,5,5,5,5,6,6,7,7,8,9]
+
+
+def generate_garbage_text(num_words: int, max_chars=None):
+    WORD_LEN_FREQ = [1, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 7, 7, 8, 9]
     res = ""
     for _ in range(num_words):
-        res += generate_garbage_word(choice(WORD_LEN_FREQ)) 
+        res += generate_garbage_word(choice(WORD_LEN_FREQ))
         res += " "
     if max_chars and len(res) > max_chars:
         return res[0:max_chars]
     return res
 
-#please pass in more than 3 tags
+
+# please pass in more than 3 tags
 def pick_random_tags(tag_ids):
     post_tags = [None, None, None]
 
@@ -46,13 +64,15 @@ def pick_random_tags(tag_ids):
 
     return post_tags
 
+
 def add_fake_tags(db, num):
     for _ in range(num):
         db.tags.insert(tag_name=generate_garbage_text(1))
 
-def add_fake_data(db, num:int):
+
+def add_fake_data(db, num: int):
     if not db().select(db.tags.id).first():
-        add_fake_tags(db, num//2)
+        add_fake_tags(db, num // 2)
 
     max_id = db().select(db.posts.id, orderby=~db.posts.id).first()
     if not max_id:
@@ -60,43 +80,48 @@ def add_fake_data(db, num:int):
     else:
         max_id = max_id.id
 
-    tag_ids = [ x['id'] for x in db().select(db.tags.id).as_list()]
+    tag_ids = [x["id"] for x in db().select(db.tags.id).as_list()]
     users = db().select(db.auth_user.id).as_list()
-    
-    for i in range(max_id+1, num+max_id+1):
+
+    for i in range(max_id + 1, num + max_id + 1):
         tags = pick_random_tags(tag_ids)
         user = choice(users)
         db.posts.insert(
-            title= f"fake title {i}",
-            body= generate_shelly_text(randint(0, 100)),
-            created_by= user['id'],
-            tag1= tags[0],
-            tag2= tags[1],
-            tag3= tags[2],
-            rating= randint(1,1000),
+            title=f"fake title {i}",
+            body=generate_shelly_text(randint(0, 100)),
+            created_by=user["id"],
+            tag1=tags[0],
+            tag2=tags[1],
+            tag3=tags[2],
+            rating=randint(1, 1000),
         )
     return
 
+
 # 0<=h<360, 0<=s,v<=1.0
-def hsv_to_rgb(h,s,v) -> str:
+def hsv_to_rgb(h, s, v) -> str:
     C = v * s
-    X = C * (1 - abs((h/60) % 2 - 1))
+    X = C * (1 - abs((h / 60) % 2 - 1))
     M = v - C
+
     def rotate(hue):
         if hue < 60:
-            return (C,X,0)
+            return (C, X, 0)
         if hue < 120:
-            return (X,C,0)
+            return (X, C, 0)
         if hue < 180:
-            return (0,C,X)
+            return (0, C, X)
         if hue < 240:
-            return (0,X,C)
+            return (0, X, C)
         if hue < 300:
-            return (X,0,C)
-        return (C,0,X)
-    R,G,B = rotate(h)
-    R,G,B = (int((R+M)*255), int((G+M) * 255), int((B+M)*255))
+            return (X, 0, C)
+        return (C, 0, X)
+
+    R, G, B = rotate(h)
+    R, G, B = (int((R + M) * 255), int((G + M) * 255), int((B + M) * 255))
     return f"#{R:x}{G:x}{B:x}"
-    
+
+
 def int_to_color(id: int) -> str:
-    return hsv_to_rgb( (id*4091) % 360, 0.8, 0.8)
+    return hsv_to_rgb((id * 4091) % 360, 0.8, 0.8)
+
