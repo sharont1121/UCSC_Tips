7c250530ab47b1a21cc1ffdac8f9f76cf76743eb
Sharon Tulman <stulman1121@gmail.com>
2022-05-25 16:48:18 -0700

    Cleaned up profile functions in vue

--- static/js/pages/profile.js
+++ static/js/pages/profile.js
@@ -12,36 +12,46 @@ function init(app) {
         LOAD_URL: LOAD_URL,
         RATE_URL: RATE_URL,
         PARAMS: PARAMS,
+
+        upload_url: null,
+        fetching_url: false,
+        uploading_image: false,
+        file_url: null,
+        file_path: null,
+        file: null,
     };
 
-    // We form the dictionary of all methods, so we can assign them
-    // to the Vue app in a single blow.
+     app.newfeedloaded = function( {search_text: search_text, tags: tags} ) {
+           let new_url = new URL(`${window.location.origin}${window.location.pathname}`) //construct url without any params
+            if(search_text){
+                new_url.searchParams.append("search", search_text);
+            }
+            if(tags){
+                new_url.searchParams.append("tags", JSON.stringify(tags))
+            }
+            window.history.pushState({},"", new_url);
+    };
+
+    app.newpostactive = function(id) {
+        let url = new URL(window.location.href);
+        if(url.searchParams.has("selectedid")){
+            url.searchParams.delete("selectedid");
+        }
+        url.searchParams.append("selectedid", id);
+        window.history.replaceState({},"", url);
+    };
+
+    app.methods ={
+        newfeedloaded: app.newfeedloaded,
+        newpostactive: app.newpostactive,
+    };
+
 
     // This creates the Vue instance.
     app.vue = new Vue({
         el: "#vue-target",
         data: app.data,
-        methods:  {
-            newpostactive: function(id) {
-                let url = new URL(window.location.href);
-                if(url.searchParams.has("selectedid")){
-                    url.searchParams.delete("selectedid");
-                }
-                url.searchParams.append("selectedid", id);
-                window.history.replaceState({},"", url);
-            },
-            newfeedloaded: function( {search_text: search_text, tags: tags} ){
-                let new_url = new URL(`${window.location.origin}${window.location.pathname}`) //construct url without any params
-                if(search_text){
-                    new_url.searchParams.append("search", search_text);
-                }
-                if(tags){
-                    new_url.searchParams.append("tags", JSON.stringify(tags))
-                }
-                window.history.pushState({},"", new_url);
-
-            }
-        },
+        methods: app.methods,
     });
 
     // And this initializes it.
